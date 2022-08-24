4231098b3b875326c0ce5a2c56d2171f3e1616a4
Vladimir Klimov <vladimir.klimov@stepik.org>
2022-08-24 15:45:07 +0000

    Refactor your dim_customers Model

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,6 +10,11 @@ orders as (
 
 ),
 
+fct_orders as (
+
+    select * from {{ ref('fct_orders') }}
+),
+
 customer_orders as (
 
     select
@@ -25,6 +30,19 @@ customer_orders as (
 
 ),
 
+customer_lifetime as (
+
+    select
+        customer_id,
+
+        sum(amount) as lifetime_value
+
+    from fct_orders
+
+    group by 1
+
+),
+
 final as (
 
     select
@@ -33,11 +51,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_lifetime.lifetime_value, 0) as lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join customer_lifetime using (customer_id)
 
 )
 
